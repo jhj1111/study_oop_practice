281827a707f4202ebeb71835b9e6c2e2579599a5
jhj1111 <happyijun@naver.com>
2025-04-08 16:00:27 +0900

    add tanker

--- lol_ws/character.py
+++ lol_ws/character.py
@@ -1,5 +1,6 @@
 # 나무위키 : https://namu.wiki/w/%EB%A6%AC%EA%B7%B8%20%EC%98%A4%EB%B8%8C%20%EB%A0%88%EC%A0%84%EB%93%9C/%EC%B1%94%ED%94%BC%EC%96%B8/%EC%97%AD%ED%95%A0%EA%B5%B0
-from stats import *
+from stats import CharacterStats
+import json, yaml
 
 class Character(CharacterStats):
     def __init__(self):
@@ -39,6 +40,14 @@ class Fighter(Character):
 
 class Mage(Character):
     def __init__(self, class_group: str='mage'):
+        super().__init__()
+        self.class_group = class_group
+
+    def __repr__(self):
+        return f"{self.class_group} class"
+    
+class Tanker(Character):
+    def __init__(self, class_group: str='tanker'):
         super().__init__()
         self.class_group = class_group
 
