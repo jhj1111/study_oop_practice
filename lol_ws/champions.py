1281fb6d1cef148d8a76eaf6d01c53c7fbe3003b
jhj1111 <happyijun@naver.com>
2025-04-08 16:01:50 +0900

    init class champions

---
+++ lol_ws/champions.py
@@ -0,0 +1,7 @@
+# 롤 인벤: https://lol.inven.co.kr/dataninfo/champion/
+from character import Fighter, Mage, Tanker
+
+class malphite(Tanker):
+    def __init__(self):
+        super().__init__()
+        
